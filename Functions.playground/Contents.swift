b4a7a3c811a3cd3529b6009522749dc4963f59c0
DominationD <dominationdnyc@yahoo.com>
2017-04-10 15:18:21 -0400

    practice

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -2,7 +2,7 @@ func printAge(name: String) {
     print("\(name) is 29")
 }
 
-printAge("Adam")
+printAge(name: "Adam")
 
 
 func happyBirthday(age: Int) {
@@ -23,15 +23,15 @@ func badGetAge(name: String) -> Int {
 
 
 let friend = "Emily"
-var friendAge = getAge(friend)
-happyBirthday(friendAge)
+var friendAge = getAge(name: friend)
+happyBirthday(age: friendAge)
 
 
 func birthdayGreeting(age: Int) -> String {
     return "Happy birthday #\(age)!"
 }
 
-var greeting = birthdayGreeting(30)
+var greeting = birthdayGreeting(age: 30)
 print(greeting)
 
 
@@ -42,4 +42,31 @@ func getAgeAndCongratulate(name: String) -> Int {
 }
 
 let friend2 = "Pete"
-var friend2Age = getAgeAndCongratulate(friend2)
+var friend2Age = getAgeAndCongratulate(name: friend2)
+
+
+func sleep() {
+    
+    print("Zzzzz")
+}
+
+func sleep(at time: String) {
+    print("Go to Sleep at \(time)")
+    
+}
+
+sleep(at: "Five OCLOCK")
+
+func blah(bloop: String, ghghgjj: Int) -> Double {
+    
+    return 5.0
+    
+}
+
+func perform(action: (String) -> Void) {
+    
+    action("Bread")
+}
+
+perform(action: sleep)
+
